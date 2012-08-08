1f6794637b10bb0ce56227a2a019ef67f13ea1a2
Fabien Brooke <fsbrookedev@gmail.com>
2012-08-08 21:57:05 +1200

    First commit

---
+++ scripts/animalsounds.js
@@ -0,0 +1,35 @@
+// Hash associating words to their sounds
+(function() {
+	var animalSounds = {
+		"cluck": {
+			"url": "http://soundcloud.com/olpc-samples/chicken2-wav",
+			"css": {
+				"color": "#b6a674"
+			}
+		},
+		"woof": {
+			"url": "https://soundcloud.com/sfxsource-sound-effects/sfxsource-com-dog-bark-sound-effect-mp3",
+			"css": {
+				"color": "brown"
+			}
+		},
+		"gobble": {
+			"url": "https://soundcloud.com/cmqueen/swsfx_turkey-gobble",
+			"css": {
+				"color": "#5d91b6"
+			}
+		},
+		"moo": {
+			"url": "https://soundcloud.com/boyley-1/moo",
+			"css": {
+				"color": "#4e44b6"
+			}
+		}
+	};
+	if (typeof SOUNDS !== "undefined") {
+		jQuery.extend(SOUNDS, animalSounds);
+	}
+	else {
+		SOUNDS = animalSounds;
+	}
+}());
